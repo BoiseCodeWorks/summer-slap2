08e153084dde33a41c16554fb22ef89e7d1cec01
Jake Overall <joverall22@gmail.com>
2018-08-08 10:47:41 -0600

    Noah McConnell

--- app/components/SlapService.js
+++ app/components/SlapService.js
@@ -1,3 +1,21 @@
-import "../models/Target.js"
+import Target from "../models/Target.js"
+let scarCrow = new Target("scarcrow", 100, "//placehold.it/200x200", 10, 15, 20, 50)
+class SlapService {
+  constructor() {
 
+  }
+  attack(type) {
+    scarCrow.attack(type)
+  }
+  get Target() {
+    return {
+      health: scarCrow.health,
+      name: scarCrow.name,
+      hits: scarCrow.hits,
+      img: scarCrow.img,
+      dead: scarCrow.dead
+    }
+  }
+}
 console.log("hello from SlapService.js")
+export default SlapService
