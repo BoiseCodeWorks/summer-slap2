ff328519a79dde2bd1fd57d7498cacd65061d9f1
Jake Overall <joverall22@gmail.com>
2018-08-08 10:28:39 -0600

    push - BOB Chace

--- app/models/Target.js
+++ app/models/Target.js
@@ -1,1 +1,33 @@
 console.log("hello from Target.js")
+
+
+class Target {
+  constructor(name, health, img, slapdmg, punchdmg, kickdmg, specialdmg) {
+    this.name = name || ""
+    this.health = health || 100
+    this.img = img || ""
+    this.damages = {
+      slap: slapdmg,
+      punch: punchdmg,
+      kick: kickdmg,
+      special: specialdmg
+    }
+    this.dead = false
+    this.hits = 0
+  }
+  attack(type) {
+    if (this.dead) {
+      return
+    }
+    if (this.damages[type]) {
+      this.health -= this.damages[type]
+      this.hits++
+        if (this.health <= 0) {
+          this.health = 0
+          this.dead = true
+        }
+    }
+  }
+}
+
+export default Target
